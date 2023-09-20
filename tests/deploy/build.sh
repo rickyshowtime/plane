7c796a135266da52dda05583c7b484f2a9050cef
Prem chavhan <pchavhan@logiquad.com>
2023-09-20 18:46:54 +0530

    fixes

---
+++ tests/deploy/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rm .env.example
+rm Dockerfile
+rm docker-compose.yml
+cp -rf ./apiserver/* ./
+mv Dockerfile.api Dockerfile
+mv docker-compose-new.yml docker-compose.yml
+
+docker buildx build . --output type=docker,name=elestio4test/plane-apiserver:latest | docker load
---
+++ tests/space/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+rm .env.example
+rm Dockerfile
+rm docker-compose.yml
+mv .gitignore .gitignore.back
+mv .eslintrc.js .eslintrc.js.back
+mv package.json package.json.back
+mv ./space/README.md ./space/README_back.md
+cp -rf ./space/* ./
+mv Dockerfile.api Dockerfile
+mv docker-compose-new.yml docker-compose.yml
+
+docker buildx build . --output type=docker,name=elestio4test/plane-space:latest | docker load
