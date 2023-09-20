9b58757922e24831176066e87834e9cfacdad17f
Prem chavhan <pchavhan@logiquad.com>
2023-09-20 19:06:45 +0530

    fixes

--- tests/nginx/build.sh
+++ tests/nginx/build.sh
@@ -2,8 +2,7 @@
 rm .env.example
 rm Dockerfile
 rm docker-compose.yml
-cp -rf ./apiserver/* ./
-mv Dockerfile.api Dockerfile
+cp -rf ./nginx/* ./
 mv docker-compose-new.yml docker-compose.yml
 
-docker buildx build . --output type=docker,name=elestio4test/plane-apiserver:latest | docker load
+docker buildx build . --output type=docker,name=elestio4test/plane-proxy:latest | docker load
