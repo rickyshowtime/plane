f98c7589c315261b11f2156ffb598b4e8c48e38e
Prem chavhan <pchavhan@logiquad.com>
2024-01-22 15:24:00 +0530

    fix

--- tests/frontend/build.sh
+++ tests/frontend/build.sh
@@ -5,5 +5,4 @@ rm docker-compose.yml
 cp -f ./web/Dockerfile.web ./
 mv Dockerfile.web Dockerfile
 mv docker-compose-new.yml docker-compose.yml
-
 docker buildx build . --output type=docker,name=elestio4test/plane-frontend:latest | docker load
