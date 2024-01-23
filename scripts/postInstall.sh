9e1d96cc93dbd9b4822f1fa36afc1f92d8997885
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-23 12:42:29 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,4 +3,18 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 30s;
+sleep 90s;
+
+target=$(docker-compose port proxy 80)
+
+
+curl http://${target}/api/licenses/instances/admins/sign-in/ \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://plane-u353.vm.elestio.app/god-mode' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' \
+  --data-raw '{"email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
